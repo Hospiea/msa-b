e453546120b81350c1bb85ca392200e8be3fbaf7
Hospiea <155417815+Hospiea@users.noreply.github.com>
2025-07-19 08:32:22 +0900

    asdf

---
+++ src/main/kotlin/org/com/msab/MsabApplication.kt
@@ -0,0 +1,11 @@
+package org.com.msab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsabApplication
+
+fun main(args: Array<String>) {
+  runApplication<MsabApplication>(*args)
+}

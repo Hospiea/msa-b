8ea9ac8ecda4cd2786028faac4ad2ec38a70acf2
Hospiea <155417815+Hospiea@users.noreply.github.com>
2025-07-19 08:33:57 +0900

    Create HomeController.kt

---
+++ src/main/kotlin/org/com/msab/controller/HomeController.kt
@@ -0,0 +1,17 @@
+package org.com.msab.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/b")
+class HomeController (
+
+) {
+
+  @GetMapping("/")
+  fun homeHandler(): String {
+    return "Hello World From Msa B"
+  }
+}
